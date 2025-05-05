5145270dd6d0ebbea8740150233e5fe4bf7e0a85
Shobhit <shobhitss2225@gmail.com>
2025-05-06 01:19:48 +0530

    Integrate MongoDB in Project.

--- src/main/java/com/groot/journalApplication/repository/JournalEntryRepository.java
+++ src/main/java/com/groot/journalApplication/repository/JournalEntryRepository.java
@@ -1,9 +1,10 @@
 package com.groot.journalApplication.repository;
 
 import com.groot.journalApplication.entity.JournalEntry;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface JournalEntryRepository extends MongoRepository<JournalEntry, String> {
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
 
 
 }
