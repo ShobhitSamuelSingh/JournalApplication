14d56cbcaf2abd933174dae791d57356446d8d68
Shobhit <shobhitss2225@gmail.com>
2025-05-17 15:12:10 +0530

    Implemented lombok to replace getters and setters

--- src/main/java/com/groot/journalApplication/controller/JournalEntryController.java
+++ src/main/java/com/groot/journalApplication/controller/JournalEntryController.java
@@ -7,14 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/journal")
-public class JournalEntryControllerV2 {
+public class JournalEntryController {
 
     @Autowired
     private JournalEntryService journalEntryService;
